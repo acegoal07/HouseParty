1f534c4bd03cc52291af76bc0f048ba409138de6
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2024-09-29 18:34:16 +0100

    tweaks
    - Adjusted some of the database names
    - Added another auto script to detected expired parties and delete them
    - Added the design for the database
    - Updated the external resource list and also moved it into the design folder

---
+++ assets/server/autoClearExpiredSessions.php
@@ -0,0 +1,9 @@
+<?php
+include '../php/databaseConnection.php';
+
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+} else {
+   $sql = 'DELETE FROM parties WHERE party_expires_at < NOW()';
+   $conn->query($sql);
+}
--- assets/server/autoSpotifyRefreshToken.php
+++ assets/server/autoSpotifyRefreshToken.php
@@ -5,7 +5,7 @@ include '../php/spotifyAppInformation.php';
 if ($conn->connect_error) {
    die("Connection failed: " . $conn->connect_error);
 } else {
-   $sql = 'SELECT * FROM parties WHERE expires_at < NOW() + INTERVAL 5 MINUTE';
+   $sql = 'SELECT * FROM parties WHERE token_expires_at < NOW() + INTERVAL 5 MINUTE';
    $result = $conn->query($sql);
    if ($result->num_rows > 0) {
       while ($row = $result->fetch_assoc()) {
@@ -27,7 +27,7 @@ if ($conn->connect_error) {
          $result = file_get_contents($url, false, $context);
          $result = json_decode($result, true);
 
-         $sql = "UPDATE parties SET access_token = '" . $result['access_token'] . "', expires_at = NOW() + INTERVAL 1 HOUR WHERE refresh_token = '" . $row['refresh_token'] . "'";
+         $sql = "UPDATE parties SET access_token = '" . $result['access_token'] . "', token_expires_at = NOW() + INTERVAL 1 HOUR WHERE refresh_token = '" . $row['refresh_token'] . "'";
          $conn->query($sql);
       }
    }
