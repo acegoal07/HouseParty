e9d90fca51d53203b5177aa8b42b8669e4345961
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2025-01-29 11:41:15 +0000

    Makes it so automatic scripts now use the secrets file

--- assets/php/server/autoClearExpiredSessions.php
+++ assets/php/server/autoClearExpiredSessions.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: https://aw1443.brighton.domains/");
-include '../secrets.php';
+
+include __DIR__ . '/../secrets.php';
 
 // SQL statement to delete expired sessions
 // DELETE FROM parties WHERE party_expires_at <= CONVERT_TZ(UTC_TIMESTAMP(), '+00:00', 'Europe/London');
--- assets/php/server/autoSpotifyRefreshToken.php
+++ assets/php/server/autoSpotifyRefreshToken.php
@@ -1,18 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: https://aw1443.brighton.domains/");
-// Spotify API credentials
-global $spotifyClientId;
-$spotifyClientId = '67fa8a1f5eec455495394d8429fede37';
-global $spotifyClientSecret;
-$spotifyClientSecret = '282380b244c54d8dbd31a74a4a49ae8b';
-// Database credentials
-$host = 'localhost';
-$user = 'aw1443_housePartyController';
-$pass = 'qHzzdp^)D+v-WO&3z^';
-$db = 'aw1443_houseParty';
-$port = '3306';
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
+include __DIR__ . '/../secrets.php';
 
 $sql = 'SELECT * FROM parties WHERE token_expires_at <= UTC_TIMESTAMP() + INTERVAL 1 HOUR + INTERVAL 15 MINUTE';
 $result = $conn->query($sql);
