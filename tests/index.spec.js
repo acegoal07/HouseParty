6d51bb29b601819da8e47677c2c628a96edab07e
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2025-03-29 00:47:35 +0000

    Update index.spec.js

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -90,8 +90,7 @@ test.describe('House Party Index Page', () => {
             value: 'dummy_token',
             domain: '127.0.0.1',
             path: '/',
-            expires: Math.floor(Date.now() / 1000) + 3600,
-            httpOnly: false
+            expires: Math.floor(Date.now() / 1000) + 3600
          },
          {
             name: 'host_id',
@@ -99,20 +98,16 @@ test.describe('House Party Index Page', () => {
             domain: '127.0.0.1',
             path: '/',
             expires: Math.floor(Date.now() / 1000) + 3600,
-            httpOnly: false
          }
       ]);
       await page.goto(basePath, { waitUntil: 'load' });
-      await page.evaluate(() => {
-         document.cookie = "refresh_token=dummy_token; path=/; SameSite=Lax; domain=127.0.0.1; expires=" + new Date(Date.now() + 3600 * 1000).toUTCString();
-         document.cookie = "host_id=dummy_host; path=/; SameSite=Lax; domain=127.0.0.1; expires=" + new Date(Date.now() + 3600 * 1000).toUTCString();
-      });
       await page.waitForTimeout(500); // wait to make sure cookies are set
       const logoutButton = page.locator('button#logout-button');
       await logoutButton.click();
       await page.waitForTimeout(500);
       if (browserName === 'webkit') {
          const cookies = await page.evaluate(() => document.cookie);
+         console.log('Cookies in WebKit:', cookies);
          expect(cookies).not.toContain('refresh_token=dummy_token');
          expect(cookies).not.toContain('host_id=dummy_host');
       } else {
